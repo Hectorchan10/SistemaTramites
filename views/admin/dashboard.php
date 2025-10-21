eac3816f76998bde64e03cbc80e5931f463ed6f4
SETHA_501_E <samuelduguol56@gmail.com>
2025-10-21 15:07:42 -0600

    tablero avance

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,16 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tablero</title>
+    <link rel="stylesheet" href="/style/tableroadmin.css">
 </head>
 <body>
     <?php include '../sidebaradministrador.php'; ?>
     <h1>Administrador</h1>
-    
     <nav style="margin-bottom: 20px;">
     <a href="/logout.php" class="btn-logout">Cerrar sesión</a>
     </nav>
+ 
+    <div class="tarjeta-mensaje">
+        <h1>¡Bienvenido!</h1>
+        <p>Has iniciado sesión correctamente</p>
+    </div>
+
+    <div class="tablero" id="tablero">
+        <div id="total-usuarios" class="tarjeta-usuarios">
+
+        </div>
+
+        <div id="total-tramites" class="tarjeta-tramites">
+    
+        </div>
+
+        <div id="total-areas" class="tarjeta-areas">
+
+        </div>
+    </div>
+
+    
+
+
 </body>
 </html>
