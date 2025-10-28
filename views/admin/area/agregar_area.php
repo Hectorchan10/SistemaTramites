8c012074e42fc4e9a6cf50fe8a54e811d96af6c6
Angel Coyoy <furioso7s12@gmail.com>
2025-10-28 10:50:22 -0600

    CENTRADO DE APARTADO DE AREA

--- views/admin/area/agregar_area.php
+++ views/admin/area/agregar_area.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Área</title>
-    <link rel="stylesheet" href="/style/usuarios.css">
+    <link rel="stylesheet" href="/style/areas.css">
 </head>
 <body>
     <?php include '../../sidebaradministrador.php'; ?>
--- views/admin/area/areas.php
+++ views/admin/area/areas.php
@@ -62,7 +62,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Áreas</title>
-    <link rel="stylesheet" href="/style/usuarios.css">
+    <link rel="stylesheet" href="/style/areas.css">
 </head>
 
 <body>
